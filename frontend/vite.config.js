d79148bf5198231e9683344c8fe90245cedef907
Tarun08091997 <Tarunsharma080997~gmail.com>
2025-05-22 16:41:30 +0530

    Save local changes beofre pulling for multi CO problem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    port: 9000,
-    host: '192.168.124.197',
-  },
+  //  server: {
+  //   port: 9000,
+  //   host: '192.168.124.197',
+  // },
 })
